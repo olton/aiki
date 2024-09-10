5fabf41b9aea596634440fb52dbd86f9da1e5082
olton <serhii@pimenov.com.ua>
2024-09-11 00:40:46 +0300

    initial commit, created index

---
+++ astro.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://aiki.in.ua',
+    compressHTML: true,
+    markdown: {
+        remarkPlugins: []
+    },
+    integrations: [
+        mdx(),
+    ]
+});
